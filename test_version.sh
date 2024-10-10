88ccb1bdc9a8c034c527e20c49cf02025bd91d1c
Mike Bland <mbland@engflow.com>
2024-10-10 12:12:29 -0400

    WIP: Bzlmod part the sixty-fifth
    Fixed some `MODULE.bazel` declarations to re-enable a successful build
    against EngFlow/example, and added first
    `test_version/MODULE.bazel.template`.
    
    TODO(mbland): Add `scala_register_unused_deps_toolchains` to
    `scala/extensions/deps.bzl` and so forth. Whoops!
    
    Specifically, for EngFlow/example to build, I needed to update
    `MODULE.bazel` such that:
    
    - `protobuf` has a `version` attribute in its `bazel_dep()`
    
    - The `scala_config` extension exposes `io_bazel_rules_scala_config`
      unconditionally, and exposes its `SCALA_VERSION` settings in a new
      `dev_dependency = True` instance.
    
    - `io_bazel_rules_scala_guava` comes from `dev_deps`, not `scala_deps`.
    
    The new `MODULE.bazel.template` mostly works, but now I see the removal
    of `bind()` calls from `twitter_scrooge` starting to bite. Time to fix
    those...
    
    ```txt
    ERROR: no such package 'external':
      the WORKSPACE file is disabled via --noenable_workspace
    
    ERROR: .../test_version/test_scala_version_1728576570/twitter_scrooge/BUILD:22:12:
      no such package 'external':
      the WORKSPACE file is disabled via --noenable_workspace
      and referenced by '//twitter_scrooge:test_scrooge_core_version'
    
    ERROR: Analysis of target '//twitter_scrooge:test_scrooge_core_version'
      failed; build aborted: Analysis failed
    ```

--- test_version.sh
+++ test_version.sh
@@ -44,6 +44,7 @@ run_in_test_repo() {
   sed \
       -e "s%\${twitter_scrooge_repositories}%$TWITTER_SCROOGE_REPOSITORIES%" \
       WORKSPACE.template >> $NEW_TEST_DIR/WORKSPACE
+  cp MODULE.bazel.template $NEW_TEST_DIR/MODULE.bazel
   cp ../.bazelrc $NEW_TEST_DIR/.bazelrc
 
   cd $NEW_TEST_DIR
