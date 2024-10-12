mbland/rules_scala

commit 19f44ab7c96fa452577de055d49ebabc13a110eb

WIP: Bzlmod part the eighty-first
Fixed inverted conditional in the `scala_deps` extension that caused all
the srcjar tests to fail. Also tweaked some of the `compiler_srcjar`
tags to trigger failures equivalent to those configured by `WORKSPACE`.

Now all the test cases pass but two. The first one, `test_compiler_patch
2.13.0`, also currently fails on `master` using `WORKSPACE`.

The second one is `test_compiler_srcjar 2.12.16`, which tries to use a
`label` in its `compiler_srcjar` tag:

```py
scala_deps.compiler_srcjar(
    version = "2.12.16",
    label = "@scala_compiler_srcjar//jar:downloaded.jar",
)
```

Replicating the test failure by executing the following command directly
in the `dt_patches/test_dt_patches_user_srcjar` repo produces:

```txt
$ bazel build //... --repo_env=SCALA_VERSION=2.12.16 //...

ERROR: Traceback (most recent call last):
  File ".../external/rules_scala~/scala/extensions/deps.bzl",
    line 148, column 38, in _scala_deps_impl
    dt_patched_compiler_setup(version, srcjars.get(version))
  File ".../external/rules_scala~/scala/private/macros/scala_repositories.bzl",
    line 100, column 28, in dt_patched_compiler_setup
    dt_patched_compiler(

Error in repository_rule:
  no repository visible as '@scala_compiler_srcjar'
  to the repository '@@rules_scala~',
  but referenced by label '@scala_compiler_srcjar//jar:downloaded.jar'
  in attribute 'srcjar' of dt_patched_compiler
  'scala_compiler_source_2_12_16'.

  Is the module 'rules_scala' missing a bazel_dep or
  use_repo(..., "scala_compiler_srcjar")?

ERROR: Analysis of target '//dummy:dummy' failed; build aborted:
  error evaluating module extension scala_deps
  in @@rules_scala~//scala/extensions:deps.bzl
```

I'll have to think hard on that, as Bzlmod repo visibility rules seem to
stymie passing in a `Label` to a user's own repo.

scala/extensions/deps.bzl
@@ -20,11 +20,7 @@ load("//scalatest:scalatest.bzl", "scalatest_repositories")
 load("//specs2:specs2_junit.bzl", "specs2_junit_repositories")
 load("//testing/private:repositories.bzl", "testing_repositories")
 load("//twitter_scrooge:twitter_scrooge.bzl", "twitter_scrooge")
-load(
-    "@io_bazel_rules_scala_config//:config.bzl",
-    "SCALA_VERSION",
-    "SCALA_VERSIONS",
-)
+load("@io_bazel_rules_scala_config//:config.bzl", "SCALA_VERSIONS")
 
 _settings = tag_class(
     attrs = {
@@ -40,16 +36,10 @@ _settings = tag_class(
     },
 )
 
-_scala_compiler_srcjar_default_url = ("https://repo1.maven.org/maven2/org/" +
-    "scala-lang/scala-compiler/%s/scala-compiler-%s-sources.jar" % (
-        SCALA_VERSION, SCALA_VERSION
-    )
-)
-
 _compiler_srcjar = tag_class(
     attrs = {
         "version": attr.string(mandatory = True),
-        "url": attr.string(default = _scala_compiler_srcjar_default_url),
+        "url": attr.string(),
         "urls": attr.string_list(),
         "label": attr.string(),
         "sha256": attr.string(),
@@ -95,7 +85,7 @@ def _get_scala_compiler_srcjars(module_ctx):
     result = {}
 
     for srcjar in module_ctx.modules[0].tags.compiler_srcjar:
-        if srcjar not in result:
+        if srcjar in result:
             continue
 
         info = {}

dt_patches/test_dt_patches_user_srcjar/MODULE.bazel
@@ -39,22 +39,30 @@ scala_deps.settings(
     validate_scala_version = False,
 )
 
+# The `scala_deps.compiler_srcjar()` tag prevents some of the kinds of errors
+# represented in the corresponding `WORKSPACE` file, so we have to force
+# different ones. In particular, we can't use unspecified data types or kwargs,
+# or Bazel itself will error out.
+
 # Invalid
 scala_deps.compiler_srcjar(
     version = "2.12.11",
+    url = "foo",
+    urls = ["bar"],
 )
 
 # Invalid
 scala_deps.compiler_srcjar(
     version = "2.12.12",
-    label = "foo",
+    url = "foo",
+    label = "baz",
 )
 
 # Invalid
 scala_deps.compiler_srcjar(
     version = "2.12.13",
-    url = "https://repo1.maven.org/maven2/org/scala-lang/scala-compiler/2.12.13/scala-compiler-2.12.13-sources.jar",
-    label = "foo",
+    urls = ["bar"],
+    label = "baz",
 )
 
 scala_deps.compiler_srcjar(

commit 47539c37e93c68b3cdc2d01bd43cd0b89938a692

WIP: Bzlmod part the eighty-fifth
Fix `dt_patches/test_dt_patches_user_srcjar` for Scala 2.12.16, which
specifies `scala_deps.compiler_srcjar` using a `Label`.

Fixes the breakage described in commit
19f44ab7c96fa452577de055d49ebabc13a110eb.

The fix was way easier than I thought. Sometimes a good night's sleep
pays off.

scala/extensions/deps.bzl
@@ -41,7 +41,7 @@ _compiler_srcjar = tag_class(
         "version": attr.string(mandatory = True),
         "url": attr.string(),
         "urls": attr.string_list(),
-        "label": attr.string(),
+        "label": attr.label(),
         "sha256": attr.string(),
         "integrity": attr.string(),
     },
@@ -91,9 +91,12 @@ def _get_scala_compiler_srcjars(module_ctx):
         info = {}
         _add_if_not_empty(info, "url", srcjar.url)
         _add_if_not_empty(info, "urls", srcjar.urls)
-        _add_if_not_empty(info, "label", srcjar.label)
         _add_if_not_empty(info, "sha256", srcjar.sha256)
         _add_if_not_empty(info, "integrity", srcjar.integrity)
+
+        # Label values don't have a length.
+        if srcjar.label != None:
+            info["label"] = srcjar.label
         result[srcjar.version] = info
 
     return result