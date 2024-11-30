20147eb77a4478b05e293567594b8f14bef90c16
Mike Bland <mbland@engflow.com>
2024-11-30 17:51:27 -0500

    Bump `rules_java` to 7.12.2
    Updates almost all dependencies on `@bazel_tools//tools/jdk:` toolchain
    targets and `.bzl` files to corresponding targets and files in
    `@rules_java//toolchains:`. Part of #1482 and #1652.
    
    All dependencies on `@bazel_tools//tools/jdk:toolchain_type` remain,
    however, as there is not yet a corresponding target in
    `@rules_java//toolchains:`.
    
    Adds the `WORKSPACE` stanza recommended in the `rules_java` release
    notes, and removes our own calls to instantiate `rules_java` repos and
    to register `rules_java` toolchains. This commit also removes
    `test/toolchains/jdk.bzl` or `//test/toolchains:java21_toolchain` since
    we no longer need them. (I actually think we didn't need them with
    7.9.0, either, but this seems like a good point at which to remove
    them.)
    
    These changes are required to avoid build breakages when updating to
    `rules_scala` 7.10.0 and higher. All targets build and all tests pass
    under Bazel 6.5.0 and 7.4.1.
    
    ---
    
    I was on the right track in my analysis from #1619 ("Bump to
    rules_java 7.9.0 for Bazel 7 compatibility" in the message for
    commit cd22d8896f209c37be8622f1dcc9770c2dc18f48). However, I thought we
    _shouldn't_ update any targets from `@bazel_tools//tools/jdk:` to
    `@rules_java//toolchains:`. That's why I thought we were stuck on
    `rules_java` 7.9.0.
    
    However, this comment from @fmeum made me think switching to
    `@rules_java//toolchains:` actually is the preferred approach:
    
    - https://github.com/bazelbuild/rules_java/issues/214#issuecomment-2331630136
    
    So this is a potentially breaking change, but in the good kind of way,
    in that it requires an easy, future proof update.

--- test/shell/test_twitter_scrooge.sh
+++ test/shell/test_twitter_scrooge.sh
@@ -7,10 +7,10 @@ runner=$(get_test_runner "${1:-local}")
 scrooge_compile_with_jdk_11() {
     # javabase and java_toolchain parameters are deprecated and may be
     # removed in Bazel >= 5.0.0
-    bazel build --javabase=@bazel_tools//tools/jdk:remote_jdk11 \
-        --host_javabase=@bazel_tools//tools/jdk:remote_jdk11 \
-        --host_java_toolchain=@bazel_tools//tools/jdk:toolchain_java11 \
-        --java_toolchain=@bazel_tools//tools/jdk:toolchain_java11 \
+    bazel build --javabase=@rules_java//toolchains:remote_jdk11 \
+        --host_javabase=@rules_java//toolchains:remote_jdk11 \
+        --host_java_toolchain=@rules_java//toolchains:toolchain_java11 \
+        --java_toolchain=@rules_java//toolchains:toolchain_java11 \
         --javacopt='--release 11' \
         --java_language_version=11 \
         --tool_java_language_version=11 \
