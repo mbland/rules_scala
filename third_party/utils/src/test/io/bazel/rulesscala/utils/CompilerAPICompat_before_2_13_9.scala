e960ff7677d2b7e3ba6de64c8b9a946d0227f702
Mike Bland <mbland@engflow.com>
2024-12-02 21:18:09 -0500

    WIP: Bzlmod part the one hundred twenty-seventh
    Removing the stale `CompilerAPICompat` files that were superseded by
    `CommandLineParserAdapter` files in the same directory during #1634
    (commit cf40df396a1f270e1a3b1ccbb3575a861acf990e).

--- third_party/utils/src/test/io/bazel/rulesscala/utils/CompilerAPICompat_before_2_13_9.scala
+++
@@ -1,7 +0,0 @@
-package io.bazel.rulesscala.utils
-
-import scala.tools.cmd.CommandLineParser
-
-trait CompilerAPICompat {
-  def tokenize(cmd: String): List[String] = CommandLineParser.tokenize(cmd)
-}
