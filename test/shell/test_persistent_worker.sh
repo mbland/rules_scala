2fe9f2595409ee8dd4fbe23c8273c5dc0d2bef0e
Mike Bland <mbland@engflow.com>
2025-01-09 22:44:52 -0500

    Fix tests by disabling `--remote_executor`, etc.
    Both persistent workers and the `buildifier` binary run by
    `//tools:lint_check` must build and execute on the host machine.
    
    With this change, all tests pass under Bazel 7.4.1 with Bzlmod enabled,
    and with remote execution configured in `.bazelrc.{remote,user}`.

--- test/shell/test_persistent_worker.sh
+++ test/shell/test_persistent_worker.sh
@@ -5,11 +5,18 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . "${dir}"/test_helper.sh
 runner=$(get_test_runner "${1:-local}")
 
-if is_windows; then
-  #Bazel sandboxing is not currently implemented in windows 
-  PERSISTENT_WORKER_FLAGS="--strategy=Scalac=worker" 
-else
-  PERSISTENT_WORKER_FLAGS="--strategy=Scalac=worker --worker_sandboxing"
+PERSISTENT_WORKER_FLAGS=(
+  # Explicitly disable remote execution and enable C++ toolchain detection in
+  # case .bazelrc configures remote execution.
+  "--remote_executor="
+  "--extra_execution_platforms=@bazel_tools//tools:host_platform"
+  "--action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN="
+  "--host_action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN="
+  "--strategy=Scalac=worker" 
+)
+
+if ! is_windows; then
+  PERSISTENT_WORKER_FLAGS+=("--worker_sandboxing")
 fi
 
 check_persistent_worker_failure() {
@@ -20,16 +27,24 @@ check_persistent_worker_failure() {
 
 test_persistent_worker_success() {
   # shellcheck disable=SC2086
-  bazel build //test:ScalaBinary $PERSISTENT_WORKER_FLAGS
+  bazel build //test:ScalaBinary "${PERSISTENT_WORKER_FLAGS[@]}"
+}
+
+_scalac_worker_build_command() {
+  echo "build ${TARGET} ${PERSISTENT_WORKER_FLAGS[@]}"
 }
 
 test_persistent_worker_failure() {
-  action_should_fail "build //test_expect_failure/diagnostics_reporter:error_file $PERSISTENT_WORKER_FLAGS"
+  local TARGET="//test_expect_failure/diagnostics_reporter:error_file"
+  action_should_fail_with_message \
+    "ErrorFile.scala:6: error: ')' expected but '}' found." \
+    "$(_scalac_worker_build_command)"
 }
 
 test_persistent_worker_handles_exception_in_macro_invocation() {
-  command="build //test_expect_failure/scalac_exceptions:bad_macro_invocation $PERSISTENT_WORKER_FLAGS"
-  check_persistent_worker_failure "$command" | grep -q "Build failure during macro expansion"
+  local TARGET="//test_expect_failure/scalac_exceptions:bad_macro_invocation"
+  check_persistent_worker_failure "$(_scalac_worker_build_command)" |
+    grep -q "Build failure during macro expansion"
 
   RESPONSE_CODE=$?
   if [ $RESPONSE_CODE -ne 0 ]; then
@@ -39,8 +54,9 @@ test_persistent_worker_handles_exception_in_macro_invocation() {
 }
 
 test_persistent_worker_handles_stack_overflow_exception() {
-  command="build //test_expect_failure/scalac_exceptions:stack_overflow_macro_invocation $PERSISTENT_WORKER_FLAGS"
-  check_persistent_worker_failure "$command" | grep -q "Build failure with StackOverflowError"
+  local TARGET="//test_expect_failure/scalac_exceptions:stack_overflow_macro_invocation"
+  check_persistent_worker_failure "$(_scalac_worker_build_command)" |
+    grep -q "Build failure with StackOverflowError"
 
   RESPONSE_CODE=$?
   if [ $RESPONSE_CODE -ne 0 ]; then
@@ -48,7 +64,6 @@ test_persistent_worker_handles_stack_overflow_exception() {
     exit 1
   fi
 }
-
 
 $runner test_persistent_worker_success
 $runner test_persistent_worker_failure
--- test_lint.sh
+++ test_lint.sh
@@ -2,4 +2,11 @@
 
 set -eou pipefail
 
-bazel run //tools:lint_check
+bazel run \
+    --platforms=@bazel_tools//tools:host_platform \
+    --host_platform=@bazel_tools//tools:host_platform \
+    --extra_execution_platforms=@bazel_tools//tools:host_platform \
+    --action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN= \
+    --host_action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN= \
+    --remote_executor= \
+    //tools:lint_check
