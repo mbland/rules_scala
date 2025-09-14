3bdbc35794c4997e35c6d28410526907865e5b66
Mike Bland <mbland@engflow.com>
2025-09-14 16:04:42 -0400

    Fix test_env_attribute_expansion.sh on Windows
    As it turns out, if Bazel is actually `bazel.exe`, then Bash _will not_
    export its environment variables to the Bazel process on Windows. This
    means that `env_inherit` functionality will have no effect, breaking
    test_scala_test_env_attribute_with_env_inherit_and_test_env. In this
    case, we _must_ export them as Command Prompt variables.
    
    Setting `bazel_bin` in `setup_suite` and using it in
    `test_scala_binary_env_attribute_expansion` made that test case more
    portable as well.

--- test/shell/test_env_attribute_expansion.sh
+++ test/shell/test_env_attribute_expansion.sh
@@ -15,6 +15,7 @@ setup_suite() {
   setup_test_tmpdir_for_file "$original_dir" "$test_source"
   test_tmpdir="$PWD"
   cd "$original_dir"
+  bazel_bin="$(command -v bazel{,.exe})"
 }
 
 teardown_suite() {
@@ -22,10 +23,10 @@ teardown_suite() {
 }
 
 test_scala_binary_env_attribute_expansion() {
-  local bindir="$(bazel info bazel-bin)"
+  local bindir="$("$bazel_bin" info bazel-bin)"
   bindir="bazel-out/${bindir#*/bazel-out/}"
 
-  bazel run //test:EnvAttributeBinary > "${test_tmpdir}/actual.txt"
+  "$bazel_bin" run //test:EnvAttributeBinary > "${test_tmpdir}/actual.txt"
 
   printf '%s\n' \
     'LOCATION: West of House' \
@@ -40,13 +41,41 @@ test_scala_binary_env_attribute_expansion() {
   diff -u --strip-trailing-cr "${test_tmpdir}"/{expected,actual}.txt
 }
 
-test_scala_test_env_attribute_with_env_inherit_and_test_env() {
+# If Bazel is actually bazel.exe, then Bash _will not_ export its environment
+# variables to the Bazel process. In this case, we _must_ export them as
+# Command Prompt variables.
+_bazel_bat() {
+  local bazel_bat="${test_tmpdir}/bazel.bat"
+
+  printf '%s\n' \
+    'set FROM_ENV_INHERIT=inherited value' \
+    'set NOT_IN_ENV_INHERIT=inherited value' \
+    'set FROM_ENV_INHERIT_AND_ENV_ATTR=inherited value' \
+    'set FROM_ENV_INHERIT_AND_TEST_ENV=inherited value' \
+    'set FROM_ALL_SOURCES=inherited value' \
+    "bazel.exe %*" \
+    > "$bazel_bat"
+  bazel_bat="${bazel_bat#$PWD/}"
+  cmd.exe '/c' "${bazel_bat//\//\\}" "$@"
+}
+
+_bazel_bash() {
   FROM_ENV_INHERIT="inherited value" \
     NOT_IN_ENV_INHERIT="inherited value" \
     FROM_ENV_INHERIT_AND_ENV_ATTR="inherited value" \
     FROM_ENV_INHERIT_AND_TEST_ENV="inherited value" \
     FROM_ALL_SOURCES="inherited value" \
-    bazel test \
+    bazel "$@"
+}
+
+test_scala_test_env_attribute_with_env_inherit_and_test_env() {
+  local bazel='_bazel_bash'
+
+  if [[ "${bazel_bin##*.}" == 'exe' ]]; then
+    bazel='_bazel_bat'
+  fi
+
+  "$bazel" test \
     --test_env=FROM_TEST_ENV="test env value" \
     --test_env=FROM_TEST_ENV_AND_ENV_ATTR="test env value" \
     --test_env=FROM_ENV_INHERIT_AND_TEST_ENV="test env value" \
